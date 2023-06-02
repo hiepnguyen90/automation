a528249309a6f53d105547f344300597b3711919
hiepnguyen90 <johnhiepnguyen1@gmail.com>
2023-06-02 00:00:07 -0400

    initial commit' git remove add origin https://github.com/hiepnguyen90/automation.git git push
    quit
    git push -u origin main
    
    stolp
    git submodole add https://github.com/hiepnguyen90/automation.git frontend/web

---
+++ src/main/java/com/example/automation/Models/Selenium.java
@@ -0,0 +1,60 @@
+package com.example.automation.Models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "files")
+public class Selenium {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    private String scenario;
+
+    private String given;
+
+    private String whenStatement;
+
+    private String thenStatement;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getScenario() {
+        return scenario;
+    }
+
+    public void setScenario(String scenario) {
+        this.scenario = scenario;
+    }
+
+    public String getGiven() {
+        return given;
+    }
+
+    public void setGiven(String given) {
+        this.given = given;
+    }
+
+    public String getWhenStatement() {
+        return whenStatement;
+    }
+
+    public void setWhenStatement(String whenStatement) {
+        this.whenStatement = whenStatement;
+    }
+
+    public String getThenStatement() {
+        return thenStatement;
+    }
+
+    public void setThenStatement(String thenStatement) {
+        this.thenStatement = thenStatement;
+    };
+
+}
